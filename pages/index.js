db157cb828049e8c36b7f7b4b8946b3c21fd8530
dipesh <kcdipesh429@gmail.com>
2020-11-04 14:47:32 +0545

    feat:add fake json placeholder api

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,35 @@
-import Head from "next/head";
+import Head from 'next/head';
+import React,{useState,useEffect} from "react"
 
 export default function Home() {
+  const [photo, setPhoto] = useState();
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos/1')
+      .then((response) => response.json())
+      .then((json) => setPhoto(json));
+  }, []);
+
   return (
     <div>
       <Head>
-        <title>
-          Social Media Preview
-        </title>
+        <title>Social Media Preview</title>
         <meta property="og:url" content="https://team-place.com/" />
         <meta property="og:type" content="website" />
         <meta property="fb:app_id" content="2747726002141483" />
-        <meta
-          property="og:title"
-          content="Social Media Preview Working?"
-        />
+        <meta property="og:title" content={photo?.title} />
         <meta name="twitter:card" content="summary" />
         <meta
           property="og:description"
           content="Hurray!! Yes Social Media Preview is Working"
         />
-        <meta property="og:image" content={"/image.png"} />
+        <meta property="og:image" content={photo?.url} />
       </Head>
       <h2>
-        Hurray!!! The Website is Complete. Lets Try Social Media Preview Now
+        {photo?.title}
       </h2>
     </div>
   );
 }
+
+
